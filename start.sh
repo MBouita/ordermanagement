d7762359a0179f69b0a21d1455b5885fc3fb45bd
mbouita <mathias.bouita@gmail.com>
2024-04-01 16:44:55 +0800

    update start script to include google api key as param

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+docker-compose down -v
+
+if [[ $# -eq 1 ]]; then
+    GOOGLE_API_KEY=$1
+else
+    GOOGLE_API_KEY=""
+fi
+
+if [[ -n $GOOGLE_API_KEY ]]; then
+    sed -i "s/^google.api.key=.*/google.api.key=$GOOGLE_API_KEY/g" ./src/main/resources/application.properties
+fi
+
 docker-compose build
 
 docker-compose up -d
